58e22784db9e3ed3cb9a39cf9251868b3959e2fe
Bhaskar Upadhyay <bhaskarupadhyay777777@gmail.com>
2026-08-19 21:23:05 +0530

    Use supported Groq model

--- server.js
+++ server.js
@@ -666,7 +666,7 @@ app.get("/", async (req, res) => {
       await groq.chat.completions.create({
 
         model:
-          "llama-3.3-70b-versatile",
+  "llama-3.1-8b-instant",
 
         temperature: 0.75,
 
